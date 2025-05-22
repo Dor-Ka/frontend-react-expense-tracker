432017a7a1531b6c7748e4dc61a34b4f56118f3d
Dorota Karpińska <dorotakarpinska.dev@gmail.com>
2025-05-22 19:28:54 +0200

    feat: render ExpenseList in App with dummy data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import Header from "./components/Header/Header";
 import Container from "./components/Container/Container";
 import Card from "./components/Card/Card";
+import ExpenseList from "./components/ExpenseList/ExpenseList";
+import { dummyExpenses } from "./data/dummyExpenses";
+
 
 const App = () => {
   return (
     <>
       <Header />
+      <ExpenseList expenses={dummyExpenses} />
+
       <Container>
         <Card>
           <h2>Sample Card</h2>
