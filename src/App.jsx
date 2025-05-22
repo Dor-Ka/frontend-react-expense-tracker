5590e11b713ed9370d98e309786cf378e924bd7b
Dorota Karpińska <dorotakarpinska.dev@gmail.com>
2025-05-22 19:07:15 +0200

    refactor: extract styles from Header to separate file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/Header";
+import Header from "./components/Header/Header";
 import Container from "./components/Container";
 import Card from "./components/Card";
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+import { StyledHeader } from "./HeaderStyles";
+
+const Header = () => {
+  return <StyledHeader>Expense Tracker</StyledHeader>;
+};
+
+export default Header;
