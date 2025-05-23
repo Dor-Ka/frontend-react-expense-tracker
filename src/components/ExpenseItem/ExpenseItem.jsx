81d6925fbd1fc79154afc8f08bd1af497c202cb0
Dorota Karpińska <dorotakarpinska.dev@gmail.com>
2025-05-23 18:14:53 +0200

    feat: create ExpenseItem component and styles

---
+++ src/components/ExpenseItem/ExpenseItem.jsx
@@ -0,0 +1,12 @@
+import { ItemWrapper } from "./ExpenseItemStyles";
+
+const ExpenseItem = ({ title, amount, date }) => {
+  return (
+    <ItemWrapper>
+      <strong>{title}</strong> — ${amount}
+      <span>{date}</span>
+    </ItemWrapper>
+  );
+};
+
+export default ExpenseItem;
