07eae0d2d5db91ce822992687efb0c608b8f7c8a
Dorota Karpińska <dorotakarpinska.dev@gmail.com>
2025-05-27 16:21:37 +0200

    feat: add style to ExpensesFilter component

---
+++ src/features/expenses/ExpensesFilter/ExpensesFilterStyles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const FilterContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: 1rem;
+`;
+
+export const Select = styled.select`
+  padding: 0.5rem;
+  font-size: 1rem;
+`;
