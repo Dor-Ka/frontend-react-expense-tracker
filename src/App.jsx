f855bf540000eb058070abe2dc3f09394f49d4df
Dorota Karpińska <dorotakarpinska.dev@gmail.com>
2025-05-27 16:23:39 +0200

    feat: add year filter logic in App and connect with ExpensesFilter

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,37 @@
+import { useState } from "react";
 import { useExpenses } from "./features/expenses/useExpenses";
+
 import Header from "./components/Header/Header";
 import Container from "./components/Container/Container";
 import Card from "./components/Card/Card";
 import ExpenseList from "./features/expenses/ExpenseList/ExpenseList";
 import ExpenseForm from "./components/ExpenseForm/ExpenseForm";
+import ExpensesFilter from "./features/expenses/ExpensesFilter/ExpensesFilter";
 
 function App() {
-
   const { expenses, addExpense } = useExpenses();
+  const [selectedYear, setSelectedYear] = useState("2025");
+
+  const handleYearChange = (year) => {
+    setSelectedYear(year);
+  };
+
+  const filteredExpenses = expenses.filter((expense) => {
+    const expenseYear = new Date(expense.date).getFullYear().toString();
+    return expenseYear === selectedYear;
+  });
 
   return (
     <>
       <Header />
-      <ExpenseList expenses={expenses} />
-
       <Container>
         <Card>
           <ExpenseForm onAddExpense={addExpense} />
         </Card>
+
+        <ExpensesFilter selectedYear={selectedYear} onChangeYear={handleYearChange} />
+
+        <ExpenseList expenses={filteredExpenses} />
       </Container>
     </>
   );
